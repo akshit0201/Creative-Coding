566507d02d839e3a0ef8f1e4aa228365457e5651
akshit0201 <akshit0201@gmail.com>
2022-05-15 16:13:27 +0400

    Added color and random radius to circles

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 const canvasSketch = require('canvas-sketch');
 const {lerp} = require("canvas-sketch-util/math");
 const random = require('canvas-sketch-util/random')
+const {math} = require("canvas-sketch-util");
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1500, 1500 ]
 };
 
 const sketch = () => {
@@ -13,7 +14,9 @@ const sketch = () => {
       for (let y=0;y<count;++y){
         const u = count<=1?0.5: x/(count-1)
         const v = count<=1?0.5: y/(count-1)
-        points.push([u,v])
+        points.push({
+            radius: Math.abs(random.gaussian()*0.01),
+              position: [u,v]})
       }
     }
     return points
@@ -25,14 +28,20 @@ const sketch = () => {
       const margin = width *0.1
 context.fillStyle = 'white'
     context.fillRect(0,0,width,height)
-    points.forEach(([u,v]) => {
+    points.forEach(data => {
+        const {
+            radius,
+            position
+        } = data
+        const [u,v] = position
 const x = lerp(margin,width-margin,u)
       const y = lerp(margin,height-margin,v)
 
       context.beginPath()
-      context.arc(x,y,10,0,Math.PI*2)
+      context.arc(x,y,radius*width,0,Math.PI*2)
       context.lineWidth = 5
-      context.stroke()
+context.fillStyle = "green"
+    context.fill()
     })
   };
 };
