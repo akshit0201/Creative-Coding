217ca48ccd6fbbf75c9dfe2c857a0e22ebfed0da
akshit0201 <akshit0201@gmail.com>
2022-05-15 15:27:57 +0400

    Added linear interpolation

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const canvasSketch = require('canvas-sketch');
-
+const {lerp} = require("canvas-sketch-util/math");
 const settings = {
   dimensions: [ 2048, 2048 ]
 };
@@ -18,16 +18,18 @@ const sketch = () => {
     return points
   }
   const points = createGrid()
+
   return ({ context, width, height }) => {
+      const margin = width *0.175
 context.fillStyle = 'white'
     context.fillRect(0,0,width,height)
     points.forEach(([u,v]) => {
-const x = u * width
-      const y = v* height
+const x = lerp(margin,width-margin,u)
+      const y = lerp(margin,height-margin,v)
 
       context.beginPath()
-      context.arc(x,y,250,0,Math.PI*2)
-      context.lineWidth = 30
+      context.arc(x,y,100,0,Math.PI*2)
+      context.lineWidth = 20
       context.stroke()
     })
   };
