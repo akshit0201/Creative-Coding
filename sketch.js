1225924bff39f83570b4e534da4040d319a68a33
akshit0201 <akshit0201@gmail.com>
2022-05-15 18:11:25 +0400

    Added noise2D to the radius

--- sketch.js
+++ sketch.js
@@ -18,15 +18,15 @@ const sketch = () => {
       for (let y=0;y<count;++y){
         const u = count<=1?0.5: x/(count-1)
         const v = count<=1?0.5: y/(count-1)
+
         points.push({
             color: random.pick(palette),
-            radius: Math.abs(random.gaussian()*0.01),
+            radius: Math.abs(random.noise2D(u,v))*0.025,
               position: [u,v]})
       }
     }
     return points
   }
-  random.setSeed(1)
   const points = createGrid().filter(()=>random.value()>0.5)
 
   return ({ context, width, height }) => {
